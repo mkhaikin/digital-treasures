4253e8100c59955ad99a5dc3836382493c9ee8da
Michael <michael.khaikin@gmail.com>
2021-02-02 22:04:21 -0500

    create emplyee route

--- server/index.js
+++ server/index.js
@@ -9,8 +9,21 @@ app.use(express.json());
 
 // ROUTES
 
+// Create employee
+app.post('/admin/employees', async (req,res) =>{
+    try {
+        const { first_name, last_name, role, email, password} = req.body;
+        const newUser = await pool.query(
+            'INSERT INTO employees (first_name, last_name, role, email, password) VALUES($1,$2,$3,$4,$5) RETURNING *',
+            [first_name, last_name, role, email, password]
+        );
+        console.log('row was inserted. new employee created');
+        res.json(newUser.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 // Get all employees
-
 // Get all customers
 // Create customer
 app.post('/customers', async (req,res) =>{
