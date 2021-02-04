d0c3949bf58eb1bd3c8b661528dbe3ad7f6b6ca1
Michael <michael.khaikin@gmail.com>
2021-02-03 22:29:22 -0500

    fixed header style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ import React, {Fragment} from 'react';
 const Header = () => {
     return (
         <Fragment>
-            <div className="container-fluid footer">
+            <div className="container-fluid header">
                 <h4>Header</h4>
             </div>
         </Fragment>
