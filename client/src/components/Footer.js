3ea0393241346355a45af4035787e844df10272d
Michael <michael.khaikin@gmail.com>
2021-02-03 22:26:02 -0500

    footer comp

---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+const Footer = () => {
+    return (
+        <Fragment>
+            <div className="container-fluid footer">
+                <h4>Footer</h4>
+            </div>
+        </Fragment>
+    );
+};
+
+export default Footer;
