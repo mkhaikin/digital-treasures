c41d5613a26ae437740a37a4b51d4bd55cb01c1c
Michael <michael.khaikin@gmail.com>
2021-02-03 22:25:54 -0500

    header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const Header = () => {
-    return <h1>Header</h1>;
+    return (
+        <Fragment>
+            <div className="container-fluid footer">
+                <h4>Header</h4>
+            </div>
+        </Fragment>
+    );
 };
 
 export default Header;
