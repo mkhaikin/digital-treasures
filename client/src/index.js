c6c9b120e3fe29b2421279a9e39af24fe4046922
Michael <michael.khaikin@gmail.com>
2021-02-23 13:54:34 -0500

    created redux store

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
+
 import './index.css';
 import App from './App';
 
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunk)
+);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+        <App/>
+      </Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
