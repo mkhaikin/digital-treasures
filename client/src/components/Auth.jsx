26321ff88fc2ca12f71991ff0abfa3d53b17eec0
Michael <michael.khaikin@gmail.com>
2021-03-02 12:33:20 -0500

    auth component

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment, useEffect, useState} from 'react';
+
+const Auth = () => {
+    
+
+    
+    return (
+        <Fragment>
+            <button className='btn btn-info'>Login</button>
+        </Fragment>
+    )
+};
+
+export default Auth;
