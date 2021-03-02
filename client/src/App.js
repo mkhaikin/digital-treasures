2182237946e87f17fd338a26f0c40b23be398900
Michael <michael.khaikin@gmail.com>
2021-03-02 12:35:15 -0500

    cleaned app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,18 +2,18 @@ import React, {Fragment} from 'react';
 import './App.css';
 
 // Components
-import Header from './components/Header';
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
+import Header from './components/Header.jsx';
+import Dashboard from './components/Dashboard.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <Fragment>
-      <Header />
-      <Dashboard />
-      <Footer />
+      <Header/>
+      <Dashboard/>
+      <Footer/>
     </Fragment>
-  );
+  )
 }
 
 export default App;
