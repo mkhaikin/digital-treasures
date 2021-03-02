b9d6300c04b82fad7213311c56729377f849094d
Michael <michael.khaikin@gmail.com>
2021-03-02 12:35:53 -0500

    menu actions

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,2 +1,7 @@
+// Menu 
+export const SELECT_CUSTOMERS = 'SELECT_CUSTOMERS';
+export const SELECT_ORDERS = 'SELECT_ORDERS';
+
+// Employee
 export const SELECT_EMPLOYEE = 'SELECT_EMPLOYEE';
 export const ADD_EMPLOYEE = 'ADD_EMPLOYEE';
