a6a037451f72a4f9b7c54e45da48772a7784e9dd
Michael <michael.khaikin@gmail.com>
2021-03-02 12:33:42 -0500

    comments and login comp.

---
+++ client/src/components/Comments.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment, useEffect, useState} from 'react';
+
+const Comments = () => {
+
+    return (
+        <Fragment>
+            <div className="container comments">
+                <h2>Comments</h2>
+            </div>
+        </Fragment>
+    )
+};
+
+export default Comments;
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React, {Fragment, useEffect, useState} from 'react';
+
+const Login = () => {
+
+    return (
+        <Fragment>
+            <div className="container-fluid dashboard">
+                <Menu/>
+                <Main/>
+            </div>
+        </Fragment>
+    )
+};
+
+export default Login;
