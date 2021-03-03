3223990caa040e9e70ebd36f0d9b7caba353e440
Michael <michael.khaikin@gmail.com>
2021-03-03 02:02:37 -0500

    not using

--- client/src/components/SelectEmployee.jsx
+++ client/src/components/SelectEmployee.jsx
@@ -11,7 +11,7 @@ const SelectEmployee = () => {
         e.preventDefault();
         if(!lastName) return;
         dispatch(selectEmployee(lastName));
-    }
+    }   
 
     return (
         <Fragment>
