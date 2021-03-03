e347e0552cc307ff5a9bf608b78f3002888278bf
Michael <michael.khaikin@gmail.com>
2021-03-03 01:59:29 -0500

    root reducer

--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
-import fetchEmployeeReducer from './reducers/fetchEmployeeReducer';
+// import fetchEmployeeReducer from './reducers/fetchEmployeeReducer';
 import menuReducer from './reducers/menuReducer';
 
 export default combineReducers({
-    menuReducer,fetchEmployeeReducer
+    menu: menuReducer
+    // fetchEmployeeReducer
 });
