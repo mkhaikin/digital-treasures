016121e0578d423c1513b23ded9fa9ad1ae8d173
Michael <michael.khaikin@gmail.com>
2021-03-03 02:02:07 -0500

    title

---
+++ client/src/components/PageTitle.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useSelector} from 'react-redux';
+
+const PageTitle = () => {
+
+    const title = useSelector((state) => state.menu.title);
+
+    return (
+        
+        <h2>{ title === 'Employees' ? "Employees"
+            : title === 'Customers' ? 'Customers'
+            : title === 'Orders' ? "Orders"
+            : "Dashboard"
+            }</h2>
+       
+    );
+};
+
+export default PageTitle;
