5e1bbc33904ccf7f31b978e0b45d53352f73c58a
Michael <michael.khaikin@gmail.com>
2021-03-03 02:00:47 -0500

    actions

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,7 +1,16 @@
 import {SELECT_EMPLOYEE} from "./actionTypes";
 
-export const selectEmployee = (payload) => ({
-    type: SELECT_EMPLOYEE,
-    payload
-});
+import axios from 'axios';
+
+
+export const selectEmployee = () => async (dispatch, getState) => { 
+    const response = await axios.get(
+        'http://localhost:5000/admin/employees'
+    );
+
+    dispatch({
+        type: SELECT_EMPLOYEE,
+        payload: response.data
+    });
+};
 
