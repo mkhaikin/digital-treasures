1ce51a40aa86926b7a8213f37a2af424f07ff6ef
Michael <michael.khaikin@gmail.com>
2021-03-03 02:02:25 -0500

    report

---
+++ client/src/components/report.jsx
@@ -0,0 +1,14 @@
+import React, {Fragment, useEffect, useState} from 'react';
+
+const Report = () => {
+
+    return (
+        <Fragment>
+            <div className="container-fluid comments">
+                <h2>Report</h2>
+            </div>
+        </Fragment>
+    )
+};
+
+export default Report;
