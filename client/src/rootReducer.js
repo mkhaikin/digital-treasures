84039e35c2a02633572c5f8cf445db19d20133d0
Michael <michael.khaikin@gmail.com>
2021-03-05 14:16:15 -0500

    clean up

--- client/src/rootReducer.js
+++ client/src/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 // import fetchEmployeeReducer from './reducers/fetchEmployeeReducer';
 import menuReducer from './reducers/menuReducer';
+import mainReducer from './reducers/mainReducer';
+
 
 export default combineReducers({
-    menu: menuReducer
-    // fetchEmployeeReducer
+    menu: menuReducer,
+    main: mainReducer
 });
